32b2f6243594989279b5a8f7a946d96433da71bf
valentincognito <valentin.vannay@gmail.com>
2017-04-24 13:59:10 +0900

    Move Enemy Formation

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -7,14 +7,29 @@ public class EnemySpawner : MonoBehaviour {
     public GameObject enemyPrefab;
 	public float width = 10f;
 	public float height = 5f;
+	public float speed = 2f;
+
+	private float xmin;
+	private float xmax;
+	private bool movingLeft;
 
     // Use this for initialization
     void Start () {
+
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftBoundary = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+		Vector3 rightBoundary = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+		xmin = leftBoundary.x;
+		xmax = rightBoundary.x;
+
+		print (xmin+" - "+xmax);
         
 		foreach(Transform child in transform){
 			GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
 			enemy.transform.parent = child;
 		}
+
+		movingLeft = true;
     }
 
 	void OnDrawGizmos(){
@@ -23,6 +38,21 @@ public class EnemySpawner : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		
+
+		if (movingLeft) {
+			this.transform.position += Vector3.left * speed * Time.deltaTime;
+		} else {
+			this.transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+
+		float rightEdgeOfFormation = transform.position.x + (0.5f*width);
+		float leftEdgeOfFormation = transform.position.x - (0.5f*width);
+
+		if(leftEdgeOfFormation < xmin){
+			movingLeft = false;
+		}else if(rightEdgeOfFormation > xmax){
+			movingLeft = true;
+		}
 	}
+
 }
